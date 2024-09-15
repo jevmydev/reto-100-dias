f6a86b144258f8dd381db4a57cd5e388286cf4e7
Jeremy Díaz <jeremy664k@gmail.com>
2024-09-15 10:40:40 -0300

    Add eslint and prettier formats

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+const RULES = {
+    OFF: "off",
+    WARN: "warn",
+    ERROR: "error"
+};
+
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: ["eslint:recommended", "prettier"],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script"
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    rules: {
+        indent: [RULES.WARN, 4],
+        quotes: [RULES.WARN, "double"],
+        semi: [RULES.WARN, "always"],
+        "linebreak-style": RULES.OFF,
+        "react/react-in-jsx-scope": RULES.OFF,
+        "react/prop-types": RULES.OFF
+    }
+};

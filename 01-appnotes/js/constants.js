18ba703314498412f64fa4836c9d248be3c5cc8f
Jeremy Díaz <jeremy664k@gmail.com>
2024-09-18 23:03:41 -0300

    Add constants.js and change location main.js in js folder

---
+++ 01-appnotes/js/constants.js
@@ -0,0 +1,12 @@
+import { $ } from "./utils.js";
+
+export const $hero = $(".hero");
+export const $createFirstNoteBtn = $(".hero__intro__button");
+
+export const $notes = $(".notes");
+export const $listNotes = $(".notes__list");
+export const $addNoteBtn = $(".list__addnote__button");
+
+export const $createNote = $(".createnote");
+export const $formCreateNote = $(".createnote__form");
+export const $closeCreateBtn = $(".createnote__exit__button");

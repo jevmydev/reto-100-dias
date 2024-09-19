8bf051bec4e42a8de3cf3d46ddf2d5528adb2279
Jeremy Díaz <jeremy664k@gmail.com>
2024-09-18 23:03:12 -0300

    Feat save data of notes in localstorage

---
+++ 01-appnotes/js/storage.js
@@ -0,0 +1,11 @@
+export const setStorage = ({ key, value }) => {
+    const valueToString = JSON.stringify(value);
+    return localStorage.setItem(key, valueToString);
+};
+
+export const getStorage = ({ key, isJSON = false }) => {
+    const value = localStorage.getItem(key);
+    if (!value) return null;
+
+    return isJSON ? JSON.parse(value) : value;
+};

5c2e5d66871c10fa5a5c865589878a48d2cceac2
Jeremy Díaz <jeremy664k@gmail.com>
2024-09-29 21:11:21 -0300

    Update design grades gap and name file

--- 02-escala-de-notas/src/components/Grades/Grades.jsx
+++ 02-escala-de-notas/src/components/Grades/Grades.jsx
@@ -1,13 +1,13 @@
 import GradesAside from "./GradesAside";
-import GradesTables from "./GradesTables";
+import GradesTable from "./GradesTable";
 
 export const Grades = () => {
     return (
         <section>
-            <div className="max-w-screen-2xl mx-auto p-4 pt-6 flex items-center justify-between h-screen">
-                <div className="flex justify-between w-full h-full">
+            <div className="max-w-screen-2xl mx-auto p-4 pl-6 pt-6 flex items-center justify-between h-screen">
+                <div className="flex gap-16 justify-between w-full h-full">
                     <GradesAside />
-                    <GradesTables />
+                    <GradesTable />
                 </div>
             </div>
         </section>

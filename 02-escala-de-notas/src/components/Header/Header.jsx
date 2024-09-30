64ba9468b7443bd51342eedadc11996b81795931
Jeremy Díaz <jeremy664k@gmail.com>
2024-09-29 21:11:59 -0300

    Change design Header

--- 02-escala-de-notas/src/components/Header/Header.jsx
+++ 02-escala-de-notas/src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import Link from "../../elements/Link";
+
 import { FavIcon } from "../../elements/Icons";
 
 export const Header = () => {
     return (
-        <header>
+        <header className="border-b border-slate-900">
             <div className="max-w-screen-2xl mx-auto p-4 flex items-center justify-between">
-                <nav>
-                    <Link href="/" title="NoteScale">
-                        <FavIcon />
+                <nav className="flex justify-between w-full">
+                    <Link href="/" title="Escala de notas">
                         <h1 className="text-4xl font-bold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-indigo-400">Escala de notas</h1>
+                    </Link>
+                    <Link href="/" title="Escala de notas">
+                        <FavIcon />
                     </Link>
                 </nav>
             </div>

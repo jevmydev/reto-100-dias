82585efa795cb6b26eeed36632203c2d401116aa
Jeremy Díaz <jeremy664k@gmail.com>
2024-09-29 21:11:38 -0300

    Add inputGrades mock for data of inputs

---
+++ 02-escala-de-notas/src/mocks/inputGrades.js
@@ -0,0 +1,45 @@
+import { v4 as uuid } from "uuid";
+
+export const inputGrades = [
+    {
+        id: uuid(),
+        label: "Puntaje máximo",
+        name: "maxScore",
+        type: "number",
+        defaultValue: "100",
+        min: "0"
+    },
+    {
+        id: uuid(),
+        label: "Nota máxima",
+        name: "maxGrade",
+        type: "number",
+        defaultValue: "7.0",
+        min: "0"
+    },
+    {
+        id: uuid(),
+        label: "Nota mínima",
+        name: "minGrade",
+        type: "number",
+        defaultValue: "1.0",
+        min: "0"
+    },
+    {
+        id: uuid(),
+        label: "Nota aprobación",
+        name: "approvalGrade",
+        type: "number",
+        defaultValue: "4.0",
+        min: "0"
+    },
+    {
+        id: uuid(),
+        label: "Exigencia (%)",
+        name: "exigency",
+        type: "number",
+        defaultValue: "60.0",
+        min: "0",
+        max: "100"
+    }
+];

a3ff03e86507dc0e55e4a5284d1db3c6827f6d54
Jeremy Díaz <jeremy664k@gmail.com>
2024-10-05 18:27:18 -0300

    Refactor type of numbers

---
+++ 02-escala-de-notas/src/constants/defaultValues.js
@@ -0,0 +1,9 @@
+export const defaultGradeData = {
+    maxScore: 100,
+    maxGrade: 7.0,
+    minGrade: 1.0,
+    approvalGrade: 4.0,
+    exigency: 0.6,
+    increment: 1,
+    order: "increasing"
+};
--- 02-escala-de-notas/src/mocks/inputGrades.js
+++ 02-escala-de-notas/src/mocks/inputGrades.js
@@ -6,40 +6,40 @@ export const inputGrades = [
         label: "Puntaje máximo",
         name: "maxScore",
         type: "number",
-        defaultValue: "100",
-        min: "0"
+        defaultValue: 100,
+        min: 0
     },
     {
         id: uuid(),
         label: "Nota máxima",
         name: "maxGrade",
         type: "number",
-        defaultValue: "7.0",
-        min: "0"
+        defaultValue: 7.0,
+        min: 0
     },
     {
         id: uuid(),
         label: "Nota mínima",
         name: "minGrade",
         type: "number",
-        defaultValue: "1.0",
-        min: "0"
+        defaultValue: 1.0,
+        min: 0
     },
     {
         id: uuid(),
         label: "Nota aprobación",
         name: "approvalGrade",
         type: "number",
-        defaultValue: "4.0",
-        min: "0"
+        defaultValue: 4.0,
+        min: 0
     },
     {
         id: uuid(),
         label: "Exigencia (%)",
         name: "exigency",
         type: "number",
-        defaultValue: "60.0",
-        min: "0",
-        max: "100"
+        defaultValue: 60,
+        min: 0,
+        max: 100
     }
 ];

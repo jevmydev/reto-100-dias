49b860e8ff19db4e65ee7bf84b3c3559915c5ab7
Jeremy Díaz <jeremy664k@gmail.com>
2024-10-05 22:01:31 -0300

    Fix height style

--- 02-escala-de-notas/src/components/Grades/Grades.jsx
+++ 02-escala-de-notas/src/components/Grades/Grades.jsx
@@ -16,7 +16,7 @@ export const Grades = () => {
 
     return (
         <section>
-            <div className="max-w-screen-2xl mx-auto p-4 pl-6 pt-6 flex items-center justify-between h-screen">
+            <div className="max-w-screen-2xl mx-auto p-4 pl-6 pt-6 flex items-center justify-between">
                 <div className="flex gap-16 justify-between w-full h-full">
                     <GradesAside handleSubmit={handleSubmit} increment={increment} updateIncrement={updateIncrement} />
                     <GradesTable gradesChunk={gradesChunk} />

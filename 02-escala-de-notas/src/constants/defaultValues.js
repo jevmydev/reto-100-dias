70e6e4f5d6557960ee1cbe431a4f07877abe3903
Jeremy Díaz <jeremy664k@gmail.com>
2024-10-05 22:01:58 -0300

    Fix exigency constant and

--- 02-escala-de-notas/src/constants/defaultValues.js
+++ 02-escala-de-notas/src/constants/defaultValues.js
@@ -2,7 +2,7 @@ export const MAX_SCORE_DEFAULT = 100;
 export const MAX_GRADE_DEFAULT = 7.0;
 export const MIN_GRADE_DEFAULT = 1.0;
 export const APPROVAL_GRADE_DEFAULT = 4.0;
-export const EXIGENCY_DEFAAULT = 0.6;
+export const EXIGENCY_DEFAULT = 60;
 export const INCREMENT_DEFAULT = 1;
 export const ORDER_DEFAULT = { increasing: "increasing", decreasing: "decreasing" };
 
@@ -13,7 +13,7 @@ export const GRADE_DATA_DEFAULT = {
     maxGrade: MAX_GRADE_DEFAULT,
     minGrade: MIN_GRADE_DEFAULT,
     approvalGrade: APPROVAL_GRADE_DEFAULT,
-    exigency: EXIGENCY_DEFAAULT,
+    exigency: EXIGENCY_DEFAULT / 100,
     increment: INCREMENT_DEFAULT,
     order: ORDER_DEFAULT.increasing
 };

fbfba8d7e58ba2ad6d8f0e9f501fe27cc427f7e7
Jeremy Díaz <jeremy664k@gmail.com>
2024-10-05 21:49:27 -0300

    Update utils folder

--- 02-escala-de-notas/src/utils/functions.js
+++ 02-escala-de-notas/src/utils/functions.js
@@ -4,3 +4,11 @@ export function trunc(number, digits) {
 
     return match ? parseFloat(match[1]) : number.valueOf();
 }
+
+export function chunkArray(array, chunkSize) {
+    const result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+}

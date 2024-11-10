ddc3d6ddc76a501758ff3a62b8f3b478291ba8a0
Jeremy Díaz <jeremy664k@gmail.com>
2024-11-09 21:16:30 -0300

    Fix replace space for " "

--- 06-monkeytype/src/components/Typing/TypingKeyBoard.jsx
+++ 06-monkeytype/src/components/Typing/TypingKeyBoard.jsx
@@ -7,14 +7,14 @@ export function TypingKeyBoard({ keyDown, isKeyUp }) {
                 return (
                     <div className="flex flex-1 justify-center items-center gap-2 text-xs text-dark" key={`KeyRow ${index}`}>
                         {keyRow.map((key) => {
-                            const isPressed = key === keyDown && isKeyUp === true;
+                            const isPressed = key === keyDown && isKeyUp;
 
                             const classIsPressKey = isPressed ? "bg-keypress" : "bg-keys";
-                            const classIsSpaceKey = key === "space" ? "px-20" : "px-2 sm:px-4";
+                            const classIsSpaceKey = key === " " ? "px-20" : "px-2 sm:px-4";
 
                             return (
                                 <button className={`flex items-center py-3 rounded-md duration-75 ${classIsPressKey} ${classIsSpaceKey}`} key={`Key ${key}`}>
-                                    {key}
+                                    {key === " " ? "space" : key}
                                 </button>
                             );
                         })}

615850e12f904c54b8cd205c40a4d1773d58a8ed
Jeremy Díaz <jeremy664k@gmail.com>
2024-11-26 20:37:26 -0300

    Add active classname for anchor

--- 07-tesla-clone/src/elements/Anchor.jsx
+++ 07-tesla-clone/src/elements/Anchor.jsx
@@ -1,3 +1,10 @@
-export function Anchor({ href, title, target, ...props }) {
-    return <a className="flex items-center px-4 py-2" href={href} title={title} target={target} {...props} />;
+export function Anchor({ href, title, target, isActivePrimary = false, isActiveSecondary = false, isRemovePad = false, isRemoveHover = false, isIcon = false, ...props }) {
+    let anchorClass = "max-w-64	w-full flex items-center justify-center rounded transition-all ease-in-out duration-200";
+    anchorClass += !isRemovePad && !isIcon ? " px-4 py-2" : "";
+    anchorClass += isIcon ? " px-1 py-1" : "";
+    anchorClass += !isRemoveHover ? " hover:backdrop-blur-lg hover:bg-hover" : "";
+    anchorClass += isActivePrimary || isActiveSecondary ? "  border-4 focus:shadow-inner-focus" : "";
+    anchorClass += isActivePrimary ? " bg-main-blue text-white border-main-blue hover:bg-main-blue" : isActiveSecondary ? " bg-white border-white text-main-dark hover:bg-white" : "";
+
+    return <a className={anchorClass} href={href} title={title} target={target} {...props} />;
 }

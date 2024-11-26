c15f99784b2cbb7fbbaf3e412278bb9d90ee4421
Jeremy Díaz <jeremy664k@gmail.com>
2024-11-26 20:37:47 -0300

    Add classname for button

--- 07-tesla-clone/src/elements/Button.jsx
+++ 07-tesla-clone/src/elements/Button.jsx
@@ -1,3 +1,8 @@
-export function Button({ title, ...props }) {
-    return <button title={title} {...props} />;
+export function Button({ title, isRemovePad = false, isRemoveHover = false, isIcon = false, ...props }) {
+    let buttonClass = "flex items-center rounded transition-colors ease-in-out duration-200";
+    buttonClass += !isRemovePad && !isIcon ? " px-4 py-2" : "";
+    buttonClass += isIcon ? " px-1 py-1" : "";
+    buttonClass += !isRemoveHover ? " hover:backdrop-blur-lg hover:bg-hover" : "";
+
+    return <button className={buttonClass} title={title} {...props} />;
 }

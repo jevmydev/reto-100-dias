92dbddf02e2c538854258e69da576880287ebafb
Jeremy Díaz <jeremy664k@gmail.com>
2024-11-26 20:35:39 -0300

    Add extend properties

--- 07-tesla-clone/tailwind.config.cjs
+++ 07-tesla-clone/tailwind.config.cjs
@@ -4,7 +4,22 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                system: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+                "universal-sans": "'Universal Sans', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+            },
+            textColor: {
+                "main-dark": "rgb(57, 60, 65)"
+            },
+            backgroundColor: {
+                hover: "rgba(0,0,0,.05)",
+                "main-blue": "#3e6ae1"
+            },
+            boxShadow: {
+                normal: "0 1px 0 0 #fff",
+                hover: "0 2px 0 0 #fff",
+                "inner-focus": "inset 0 0 0 2px currentColor"
+            },
+            borderColor: {
+                "main-blue": "#3e6ae1"
             }
         }
     },

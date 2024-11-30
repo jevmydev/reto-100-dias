88fae5ce82c60f236b033ed7fd4dad61f56b0221
Jeremy Díaz <jeremy664k@gmail.com>
2024-11-30 20:55:34 -0300

    Feat snap pages and header scroll

--- 07-tesla-clone/src/App.jsx
+++ 07-tesla-clone/src/App.jsx
@@ -1,12 +1,19 @@
+import { useRef } from "react";
+
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
+import { useHeaderScroll } from "./hooks/useHeaderScroll";
+
 export function App() {
+    const mainRef = useRef();
+    const { isScrollingUp, isTopHeader } = useHeaderScroll(mainRef);
+
     return (
         <>
-            <Header />
-            <main>
+            <Header isTopHeader={isTopHeader} isScrollingUp={isScrollingUp} />
+            <main ref={mainRef} className="snap-y snap-mandatory scroll-smooth overflow-y-auto h-screen">
                 <Home />
             </main>
             <Footer />
